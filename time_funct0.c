50e3c4c7ca92a4a83cf106203663eeb5b3d0dd67
Ahmed Nirzar <127215501+Ahmed-Nirzar@users.noreply.github.com>
2024-04-02 21:18:48 +0600

    This program shows asctime() function.

---
+++ time_funct0.c
@@ -0,0 +1,16 @@
+// This program demonstrates asctime(). 
+
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+    struct tm *ptr;
+    time_t lt;
+
+    lt = time(NULL);
+    ptr = localtime(&lt);
+    printf(asctime(ptr));
+
+    return 0;
+}
